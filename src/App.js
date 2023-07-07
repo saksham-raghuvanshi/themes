c56b4d6459a9b7ec670ecc57903dc97512fde7f4
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-07-07 23:57:14 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import logo from "./asset/logo.png";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.removeAttribute("class");
+    document.documentElement.classList.add(theme);
+  }, [theme]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header>
+          <div className="logo">
+            <img src={logo} alt="todo Frontendgyaan logo" />
+            <span>Frontend Gyaan</span>
+          </div>
+          <div className="themeSelector">
+            <span
+              onClick={() => setTheme("light")}
+              className={theme === "light" ? "light activeTheme" : "light"}
+            ></span>
+            <span
+              onClick={() => setTheme("medium")}
+              className={theme === "medium" ? "medium activeTheme" : "medium"}
+            ></span>
+            <span
+              onClick={() => setTheme("dark")}
+              className={theme === "dark" ? "dark activeTheme" : "dark"}
+            ></span>
+            <span
+              onClick={() => setTheme("gOne")}
+              className={theme === "gOne" ? "gOne activeTheme" : "gOne"}
+            ></span>
+            <span
+              onClick={() => setTheme("gTwo")}
+              className={theme === "gTwo" ? "gTwo activeTheme" : "gTwo"}
+            ></span>
+            <span
+              onClick={() => setTheme("gThree")}
+              className={
+                theme === "gradientThree" ? "gThree activeTheme" : "gThree"
+              }
+            ></span>
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
